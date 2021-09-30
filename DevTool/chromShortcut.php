1f9572de229758e246488cdbf0fed797a7050675
Amirul <rafikhan97ctg@gmail.com>
2021-09-30 23:45:15 +0600

    chrome DevTool added

--- DevTool/chromShortcut.php
+++ DevTool/chromShortcut.php
@@ -1,2 +1,17 @@
 <!-- ctrl + pageUp = move to next tab  -->
 <!-- ctrl + pageDown = move to prev teb -->
+<!-- ctrl + shift + j = Opens chromes DevTool -->
+<!-- ctrl + j = go to downloads -->
+<!-- shift + Esc = Open chrome task meneger -->
+<!-- ctrl + d = bookMark page -->
+<!-- ctrl + t = Open a new tab -->
+<!-- Alt + F = Open menu  -->
+
+
+<!-- ctrl + w = Close the Current Tab -->
+<!-- ctrl + shift + t = Re-Open the close tab/ Move to prev working tab -->
+
+
+
+<!-- ctrl + shift + Q = Close chrom all window/tabs/everything -->
+<!-- ctrl + shist + delete = Clear browsing history -->
